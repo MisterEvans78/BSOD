38f6eca1f367711c1d8e244d83658bdf60b26c22
Evans <71824258+MisterEvans78@users.noreply.github.com>
2022-11-05 00:24:05 +0100

    upload

---
+++ Ecran de la mort/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Height = My.Computer.Screen.Bounds.Height
+        Me.Width = My.Computer.Screen.Bounds.Width
+        Cursor.Hide()
+        Me.Location = New Point(0, 0)
+    End Sub
+End Class
